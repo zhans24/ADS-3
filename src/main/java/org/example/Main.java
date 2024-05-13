dfbf5b1ef8cf76805bad1fd04575fa78fcc16f34
zhanserik <zhasikgaziz@gmail.com>
2024-05-13 19:18:44 +0500

    first

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+
+public class Main {
+    public static void main(String[] args) throws JsonProcessingException {
+
+    }
+
+}
+
